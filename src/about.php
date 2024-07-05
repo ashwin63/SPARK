7cd9e95c839ecd1b55b36bae51b06ecc43d2baa8
ashwin63 <ashwinsenthil63@gmail.com>
2024-07-05 11:51:57 -0400

    Update about.php

--- src/about.php
+++ src/about.php
@@ -72,6 +72,6 @@ Spark hospitals which is a National Accreditation board for hospitals (NABH) cer
   width="600"
   height="450"
   frameborder="0" style="border:0"
-  src="https://www.google.com/maps/embed/v1/place?key= AIzaSyAz-xPgRxIuGLJYX-Z2xjqNoSJHjMTWgCE 
+  src="https://www.google.com/maps/embed/v1/place?key=REPLACEWITHYOURKEY 
     &q=Space+Needle,Seattle+WA" allowfullscreen>
 </iframe>
